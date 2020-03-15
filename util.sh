253f3f728235a3edaac2f460880efad99a6263fa
Dong Pak <dong.pak@outlook.com>
2020-03-14 22:10:10 -0400

    Updated so it is more generic

--- util.sh
+++ util.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-ARTIFACT=churchapilib
+MAIN=churchapi
+ARTIFACT=${MAIN}lib
 VER=1.0
-LIBS=./${ARTIFACT}lib/build/libs
+LIBS=./${ARTIFACT}/build/libs
 JAR=$LIBS/$ARTIFACT-$VER.jar
 GROUP=com.churchclerk
 MVN=../../../apache-maven-3.6.1/bin/mvn
@@ -17,20 +18,20 @@ case $1 in
 
   docker)
     cd app/build/docker
-    docker build -t dongpak/churchapi:latest -f jdk8.dockerfile .
+    docker build -t dongpak/${MAIN}:latest -f jdk8.dockerfile .
     ;;
 
   run)
-    docker service create --name churchapi --network $2 --secret source=$3,target=churchclerk dongpak/addressapi:latest
+    docker service create --name ${MAIN} --network $2 --secret source=$3,target=churchclerk dongpak/${MAIN}:latest
     ;;
 
   debug)
-    docker service create --name churchapi --network $2 --secret source=$3,target=churchclerk --env APP_ARG=--debug dongpak/addressapi:latest
+    docker service create --name ${MAIN} --network $2 --secret source=$3,target=churchclerk --env APP_ARG=--debug dongpak/${MAIN}:latest
     ;;
 
   *)
     echo "util.sh build - runs the gradle build command"
-    echo "util.sh lib - installs the library file"
+    echo "util.sh lib - installs the library file(s)"
     echo "util.sh docker - builds the docker image"
     echo "util.sh run <network> <secret> - starts the service"
     echo "util.sh debug <network> <secret> - starts the service in debug"
